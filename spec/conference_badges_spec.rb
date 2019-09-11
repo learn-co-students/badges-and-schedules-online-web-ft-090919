0643d8b44e6c4f3c7530014820efc76a28cbe46d
Craig Christopher Messi <craig.messi@gmail.com>
2019-09-11 19:29:12 +0000

    Done.

--- spec/conference_badges_spec.rb
+++ spec/conference_badges_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe 'conference_badges' o
+describe 'conference_badges' do
 
   let(:name) {"Arel"}
   let(:attendees) {["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]}
