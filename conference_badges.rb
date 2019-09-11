7604a6b6eb562654d8cc6e6b0e9cd8aea90a94f4
Randel Jiao <randeljiao@gmail.com>
2019-09-11 02:30:58 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,11 @@
-# Write your code here.
+def batch_badge_creator(array)
+  array.each {|name| puts "Hello, my name is #{name}."
+end
+
+def assign_rooms(array)
+  counter = 0
+  while counter < array
+  name = array
+  room_number = counter + 1
+  puts "Hello #{name}! You'll be assigned room #{room_number}!"
+end
