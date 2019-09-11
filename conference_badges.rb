67fd822908529b59ca300235dcc844985be3e3fa
Craig Christopher Messi <craig.messi@gmail.com>
2019-09-11 20:00:22 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -17,10 +17,10 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  batch_badge_creator(attendees).each do |badge|
-    puts badge
+  batch_badge_creator(attendees).each do |tag|
+    puts tag
   end
-  assign_rooms(attendees).each do |assignment|
-    puts assignment
+  assign_rooms(attendees).each do |room|
+    puts room
   end
 end
