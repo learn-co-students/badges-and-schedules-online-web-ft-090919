95946705296bade91e49ac2afa33382d0743a03b
Alyssa Yates <bosslys@icloud.com>
2019-09-12 17:52:42 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,33 @@
-# Write your code here.
+def badge_maker(name)
+ "Hello, my name is #{name}."
+end
+
+
+def batch_badge_creator(attendees)
+  array = []
+  attendees.each do |name|
+    array << badge_maker(name)
+  end
+  array
+end
+
+
+def assign_rooms(attendees)
+  num = 1
+  array = []
+  attendees.each do |name|
+    array << "Hello, #{name}! You'll be assigned to room #{num}!"
+    num += 1
+  end
+  array
+end
+
+
+def printer(attendees)
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
+  end
+  assign_rooms(attendees).each do |room|
+    puts room 
+  end
+end
